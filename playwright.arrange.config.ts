3f93c8fabbc8aae23376260c1bad47c4fe135250
Artem Sakhatskiy <artem.sakhatskiy@proton.me>
2024-02-14 22:23:02 +0000

    feat: add projects handling

--- playwright.arrange.config.ts
+++ playwright.arrange.config.ts
@@ -1,4 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 import configBase from './playwright.config';
 import { getReporters } from './src';
 
@@ -11,6 +12,26 @@ const config: PlaywrightTestConfig = {
       project: 'arranged-tests',
       allowTelemetry: true,
     }),
+  ],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'iphone',
+      use: {
+        ...devices['iPhone 13 Mini'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
   ],
 };
 
